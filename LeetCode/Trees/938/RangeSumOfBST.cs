147feb46c5f6f0a3cc0d0a83c19a172d4403664c
Eric Anderson <anderson.eric.joseph@gmail.com>
2021-02-20 22:46:07 -0700

    LC #938 RangeSumOfBST

---
+++ LeetCode/Trees/938/RangeSumOfBST.cs
@@ -0,0 +1,44 @@
+// LeetCode #938
+// https://leetcode.com/problems/range-sum-of-bst/
+//
+// Given the root node of a binary search tree, return the sum of values of all nodes with a value in the range [low, high].
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// This algorithm recurses through our tree and sums as it goes where the value of the targeted node is in range.
+// No additional recursive method is necessary in this case since our original method returns an int.
+
+// Time: O(n)
+// Space: O(h) where h is the height of the tree
+public class Solution {
+    public int RangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) return 0;
+
+        int value = 0;
+        if (root.val >= low && root.val <= high) {
+            value = root.val;
+        }
+
+        if (root.left != null && root.right != null) {
+            return RangeSumBST(root.left, low, high) + RangeSumBST(root.right, low, high) + value;
+        }
+
+        if (root.left != null) {
+            return RangeSumBST(root.left, low, high) + value;
+        }
+
+        return RangeSumBST(root.right, low, high) + value;
+    }
+}
