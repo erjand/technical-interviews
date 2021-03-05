c23f3e4c9e81591bba454199034aed438bccde29
Eric Anderson <anderson.eric.joseph@gmail.com>
2021-03-05 11:45:18 -0700

    LC #575 DistributeCandies

--- Problems/LeetCode/StringsArraysAndLists/575/DistributeCandies.cs
+++ Problems/LeetCode/StringsArraysAndLists/575/DistributeCandies.cs
@@ -5,3 +5,24 @@
 // The doctor advised Alice to only eat n / 2 of the candies she has (n is always even). 
 // Alice likes her candies very much, and she wants to eat the maximum number of different types of candies while still following the doctor's advice.
 // Given the integer array candyType of length n, return the maximum number of different types of candies she can eat if she only eats n / 2 of them.
+
+// Time: O(n)
+// Space: O(n)
+public class Solution {
+    public int DistributeCandies(int[] candyType) {
+        int numberEaten = 0;
+        int maxCanEat = candyType.Length / 2;
+        HashSet<int> typesEaten = new HashSet<int>();
+        
+        for (int i = 0; i < candyType.Length; i++) {
+            if (numberEaten < maxCanEat) {
+                if (!typesEaten.Contains(candyType[i])) {
+                    typesEaten.Add(candyType[i]);
+                    numberEaten++;
+                }
+            }
+        }
+        
+        return typesEaten.Count;
+    }
+}
