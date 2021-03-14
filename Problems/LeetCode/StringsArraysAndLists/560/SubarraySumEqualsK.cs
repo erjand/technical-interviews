af3b9ce2bc4bf194d95658d9aee8070c3c45b99b
Eric Anderson <anderson.eric.joseph@gmail.com>
2021-03-14 17:43:55 -0600

    LC #560 SubarraySumEqualsK

---
+++ Problems/LeetCode/StringsArraysAndLists/560/SubarraySumEqualsK.cs
@@ -0,0 +1,29 @@
+// LeetCode #560
+// https://leetcode.com/problems/subarray-sum-equals-k/
+//
+// Given an array of integers nums and an integer k, return the total number of continuous subarrays whose sum equals to k.
+
+// Time: O(n^2)
+// Space: O(1)
+public class Solution {
+    public int SubarraySum(int[] nums, int k) {
+        int subArrayCount = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            sum = nums[i];
+            if (sum == k) {
+                subArrayCount++;
+            }
+            
+            for (int j = i + 1; j < nums.Length; j++) {
+                sum += nums[j];
+                
+                if (sum == k) {
+                    subArrayCount++;
+                }
+            }
+        }
+        
+        return subArrayCount;
+    }
+}
