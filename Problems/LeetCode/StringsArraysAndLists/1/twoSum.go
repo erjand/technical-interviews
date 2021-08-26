fd3fec75cc9ad59204db678f3d63a97251b6ca57
Eric Anderson <anderson.eric.joseph@gmail.com>
2021-08-26 09:24:28 -0600

    LC #1 twoSum.go

---
+++ Problems/LeetCode/StringsArraysAndLists/1/twoSum.go
@@ -0,0 +1,22 @@
+// LeetCode #1
+// https://leetcode.com/problems/two-sum/
+//
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+func twoSum(nums []int, target int) []int {
+	var numHash = make(map[int]int)
+
+	for i := 0; i < len(nums); i++ {
+		value, hasComplement := numHash[target-nums[i]]
+
+		if hasComplement {
+			return []int{i, value}
+		}
+
+		numHash[nums[i]] = i
+	}
+
+	return []int{}
+}
