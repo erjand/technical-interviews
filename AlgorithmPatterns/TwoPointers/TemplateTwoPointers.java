62e9086108d1543238f5e51e3c6302fa1bf8f903
Eric Anderson <anderson.eric.joseph@gmail.com>
2021-11-29 09:34:31 -0700

    Add AlgorithmPatterns and templates for Sliding Window and Two Pointers

---
+++ AlgorithmPatterns/TwoPointers/TemplateTwoPointers.java
@@ -0,0 +1,18 @@
+// A template for implementing the Two Pointers algorithm pattern.
+
+public static int twoPointers(int[] arr) {
+    int left = 0;
+    int right = arr.length - 1;
+
+    while (left < right) {
+        if (foo) {
+            left++;
+        } else if (bar) {
+            right--;
+        } else if (successCondition) {
+            return 1;
+        }
+    }
+
+    return -1;
+}
