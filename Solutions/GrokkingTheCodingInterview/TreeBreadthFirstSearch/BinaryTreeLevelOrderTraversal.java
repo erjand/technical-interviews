001fd625e60ce4d1ba445c2b137fa39f038e535f
Eric Anderson <anderson.eric.joseph@gmail.com>
2021-12-28 16:11:46 -0700

    Grokking the Coding Interview: BinaryTreeLevelOrderTraversal.java

---
+++ Solutions/GrokkingTheCodingInterview/TreeBreadthFirstSearch/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,53 @@
+// From the educative.io course "Grokking the Coding Interview".
+// Pattern: Tree Breadth First Search
+// Problem: Binary Tree Level Order Traversal (easy)
+
+// Problem Statement:
+// Given a binary tree, populate an array to represent its level-by-level traversal. 
+// You should populate the values of all nodes of each level from left to right in separate sub-arrays.
+
+// Sample TreeNode class.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+// Time: O(n) where n is the number of nodes in the tree.
+// Space: O(w) where w is the maximum width of the tree.
+public static List<List<Integer>> traverse(TreeNode root) {
+    List<List<Integer>> result = new ArrayList<List<Integer>>();
+    
+    if (root == null) {
+        return result;
+    }
+
+    Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
+    nodeQueue.offer(root);
+
+    while (!nodeQueue.isEmpty()) {
+        int levelNodeCount = nodeQueue.size();
+        List<Integer> levelList = new ArrayList<Integer>();
+        
+        // Process all the nodes in this level.
+        for (int i = 0; i < levelNodeCount; i++) {
+            TreeNode currentNode = nodeQueue.poll();
+            levelList.add(currentNode.val);
+            
+            if (currentNode.left != null) {
+                nodeQueue.offer(currentNode.left);
+            }
+            if (currentNode.right != null) {
+                nodeQueue.offer(currentNode.right);
+            }
+        }
+        
+        result.add(levelList);
+    }
+
+    return result;
+}
