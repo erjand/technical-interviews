42994f2365a7392ba9f599e610dde78d0f9cf1c0
Eric Anderson <anderson.eric.joseph@gmail.com>
2022-01-10 15:42:11 -0700

    TwoNumbersAddToN.java

---
+++ Solutions/DataStructures/Array/TwoNumbersAddToN.java
@@ -0,0 +1,19 @@
+// Problem Statement:
+// Implement a method which will take a number n, and an array arr as input and returns an array of two integers 
+// that add up to n in an array. If no pair is found then return the array.
+
+public static int[] findSum(int[] arr, int n) {
+    HashSet<Integer> numHash = new HashSet<Integer>();
+
+    for (int i : arr) {
+        int complement = n - i;
+
+        if (numHash.contains(complement)) {
+            return new int[] {i, complement};
+        } else {
+            numHash.add(i);
+        }
+    }
+
+    return arr;
+}
